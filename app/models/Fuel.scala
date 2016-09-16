dca09b471dc8476701b02418ab5624fedb4b672f
Inaki Anduaga <inaki.anduaga@tado.com>
2016-09-17 01:26:59 +0200

    #8: Added helper methods on Fuel enumeration

--- app/models/Fuel.scala
+++ app/models/Fuel.scala
@@ -7,4 +7,9 @@ object Fuel extends Enumeration {
   type Fuel = Value
   val Gasoline = Value("Gasoline")
   val Diesel = Value("Diesel")
+
+  def fromString(s: String): Option[Fuel.Value] = values.find(_.toString == s)
+
+  def isFuelType(s: String): Boolean = fromString(s).isDefined
+
 }
